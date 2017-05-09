cfe5072f91bd46dfe84db92d9f45d1e5f7609ca8
Julius Kabangi <kabangijulius@gmail.com>
2017-05-09 08:23:24 +0300

    Update to react-native 0.43.4

--- ios/RCTTwilioChat/RCTTwilioAccessManager.m
+++ ios/RCTTwilioChat/RCTTwilioAccessManager.m
@@ -7,11 +7,11 @@
 //
 
 #import "RCTTwilioAccessManager.h"
-#import "RCTBridge.h"
+#import <React/RCTBridge.h>
 #import "RCTConvert+TwilioChatClient.h"
-#import "RCTUtils.h"
+#import <React/RCTUtils.h>
 #import "RCTTwilioChatClient.h"
-#import "RCTEventDispatcher.h"
+#import <React/RCTEventDispatcher.h>
 
 @interface RCTTwilioAccessManager() <TwilioAccessManagerDelegate>
 @end
